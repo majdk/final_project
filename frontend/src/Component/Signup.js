31e178204fe87e1a442100f97cd9a913400bd7dd
majdk <smajd11@csf.technion.ac.il>
2020-01-17 18:51:12 +0200

    test

--- frontend/src/Component/Signup.js
+++ frontend/src/Component/Signup.js
@@ -1,24 +1,210 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from "axios";
 
 
-class Signup extends Component {
-    state= {
-        users: []
+const useStyles = theme => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+});
+
+export const signup = user => {
+    axios.defaults.withCredentials = true;
+    return axios
+        .post('http://127.0.0.1:5000/user/new', user)
+        .then(response => {
+            localStorage.setItem('usertoken', response.data)
+            return response.data
+        })
+        .catch(err => {
+            console.log(err)
+            return 'error'
+        })
+}
+
+class SignUp extends Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: '',
+            username: '',
+            bio: '',
+            firstname: '',
+            lastname: '',
+            errors: {
+                email: '',
+                password: '',
+                username: '',
+                bio: '',
+                firstname: '',
+                lastname: '',
+            },
+            invalid: 0
+
+        };
+        // console.log('asdasdsa')
+        this.onChange = this.onChange.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+        e.preventDefault();
+        // console.log('asdasdsa');
     }
 
 
-
-  render() {
-    return (
-      <div className="container">
-        <div className="jumbotron mt-4">
-          <div className="col-sm-8 mx-auto">
-            <h1 className="text-center">Signup</h1>
-          </div>
-        </div>
-      </div>
-    )
-  }
+    onSubmit(e) {
+        e.preventDefault();
+        this.setState({invalid: 0});
+        // localStorage.setItem('user', 'majd');
+        const user = {
+            email: this.state.email,
+            password: this.state.password,
+            username: this.state.username,
+            bio: this.state.bio,
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+        }
+        signup(user).then(res => {
+            if (res != 'error') {
+                this.props.history.push(`/`)
+            }
+            else {
+                this.setState({invalid: 1});
+            }
+        })
+    }
+// export default function SignIn() {
+    render() {
+        // console.log('asdasdsa');
+        const { classes } = this.props;
+        return (
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Typography component="h1" variant="h4">
+                        Sign up
+                    </Typography>
+                    <form className={classes.form} noValidate>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="user_name"
+                                    label="Username"
+                                    name="username"
+                                    autoComplete="user_name"
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="fname"
+                                    name="firstname"
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastname"
+                                    autoComplete="lname"
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    variant="outlined"
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="current-password"
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    fullWidth
+                                    id="outlined-textarea"
+                                    label="Bio"
+                                    multiline
+                                    name="bio"
+                                    variant="outlined"
+                                    onChange={this.onChange}
+                                />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justify="flex-end">
+                            <Grid item>
+                                <Link href="/login" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        )
+    }
 }
 
-export default Signup
+export default withStyles(useStyles)(SignUp)
