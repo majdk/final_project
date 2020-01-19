59b9adae29a6a1089f71c671d5e2cfffe5bdc5b4
mohamad abd alazez <abdalazez@campus.technion.ac.il>
2020-01-19 21:54:43 +0200

    changed the api to be more restful

--- backend/routes.py
+++ backend/routes.py
@@ -44,35 +44,29 @@ def forbidden(error):
 
 @app.route("/user", methods=['POST'])
 def register():
-    print("he is here")
     if current_user.is_authenticated:
-        print("line 49")
-        abort(400)
+        abort(403)
     data = request.get_json()
-    print(data)
     if not data or not 'password' in data or not 'username' in data or not 'firstname' in data \
             or not 'lastname' in data or not 'email' in data or not 'bio' in data:
-        print("aborted after first check")
         abort(400)
     # if 'file' not in request.files:
     #     abort(400)
-
     check_user = User.query.filter_by(email=data['email']).first()
     if check_user:
-        return 'Email Taken'
+        return make_response((jsonify({'error': 'email taken'})), 400)
     check_user = User.query.filter_by(username=data['username']).first()
     if check_user:
-        return 'Username Taken'
+        return make_response((jsonify({'error': 'username taken'})), 400)
     # file = request.files['file']
     # picture_saved_name = save_picture(file)
     hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
     user = User(username=data['username'], first_name=data['firstname'], last_name=data['lastname'],
                 email=data['email'],
                 password=hashed_password, bio=data['bio'])
-    print("oh we got here")
     db.session.add(user)
     db.session.commit()
-    return 'Created'
+    return make_response((jsonify({'ok': 'account created'})), 200)
 
 # @app.route("/user/savepic", methods=['POST'])
 # def savepic():
@@ -84,50 +78,38 @@ def register():
 #     return picture_saved_name
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=['POST'])
 def login():
-    print("at login : ")
     if current_user.is_authenticated:
-        # logout_user()
-        print("we are here")
-        abort(404)
+        abort(403)
     user_data = request.get_json()
     if not user_data or not 'password' in user_data or not 'email' in user_data:
-        print("before 400 1")
         abort(400)
 
     user = User.query.filter_by(email=user_data['email']).first()
 
-    # print(" the user is :" )
-    # print(user)
-    # print("user data is :")
-    # print(user_data)
-    # print(user.password)
-    # print(user_data['password'])
     if user and bcrypt.check_password_hash(user.password, user_data['password']):
-        print("im login")
         login_user(user, remember=True)
         access_token = create_access_token(identity={'id': user.id})
         result = access_token
     else:
-        print('Something wrong')
         abort(400)
 
     return result
 
 
-@app.route("/user/editprofile", methods=['POST'])
+@app.route("/user/profile", methods=['POST'])
 @login_required
 def editprofile():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     if not user:
         abort(404)
     user_data = request.get_json()
     if 'id' in user_data or 'username' in user_data or 'email' in user_data:
-        abort(404)
+        abort(400)
     # if 'file' not in request.files:
     #     picture_path=user.image_file
     # else:
@@ -140,40 +122,39 @@ def editprofile():
     user.bio = user_data['bio']
     # user.image_file=picture_path
     db.session.commit()
-    return 'done'
+    return make_response((jsonify({'ok': 'profile edited'})), 200)
 
 
-@app.route("/logout", methods=['GET'])
+@app.route("/logout", methods=['POST'])
 @login_required
 def logout():
-    print('logging out')
     logout_user()
-    return 'Logged Out', 201
+    return make_response((jsonify({'ok': 'Logged out'})), 201)
 
 
-@app.route("/users/<int:user_id>", methods=['GET'])
+@app.route("/user/<int:user_id>", methods=['GET'])
 @login_required
 def getUser(user_id):
     current_user_id = current_user.get_id()
     user = User.query.filter_by(id=user_id).first()
     if not user:
-        print("why not found")
         abort(404)
     following = user.followers.filter_by(follower_id=current_user_id).first()
     if not following:
         isFollowing = False
     else:
         isFollowing = True
-    # image_file = url_for('static', filename='profile_pics/' + user.image_file)
+
     return make_response(jsonify(
         {'user_id': user.id, 'username': user.username, 'first_name': user.first_name, 'last_name': user.last_name,
          'gender': user.gender, 'birth_date': user.birth_date, 'email': user.email,
          'bio': user.bio,
          'followers': len(user.followers.all()),
-         'followed': len(user.followed.all()), 'isfollowing': isFollowing}), 200)
+         'followed': len(user.followed.all()), 'isfollowing': isFollowing
+         ,'image':user.image_file}), 200)
 
 
-@app.route("/users/posts/<int:user_id>", methods=['GET'])
+@app.route("/user/posts/<int:user_id>", methods=['GET'])
 @login_required
 def getUserPosts(user_id):
     current_user_id = current_user.get_id()
@@ -186,13 +167,13 @@ def getUserPosts(user_id):
     return make_response(jsonify(json_list), 200)
 
 
-@app.route("/user/addpost", methods=['POST'])
+@app.route("/user/post", methods=['POST'])
 @login_required
 def addPost():
     data = request.get_json()
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     if not user:
         abort(404)
@@ -207,30 +188,24 @@ def addPost():
 def follow(followed_user_id):
     user_id = current_user.get_id()
     if not user_id:
-        print("160")
         abort(404)
     user = User.query.filter_by(id=user_id).first()
     followed_user = User.query.filter_by(id=followed_user_id).first()
     if not user or not followed_user:
-        print(165)
         abort(404)
     follow_instance = Follow(follower_id=user_id, followed_id=followed_user_id)
     db.session.add(follow_instance)
     db.session.commit()
-    user = User.query.filter_by(id=user_id).first()
-    followed_user = User.query.filter_by(id=followed_user_id).first()
-    return make_response(jsonify(
-        {'myuserfollowList': len(user.followed.all()), 'theotheruserfollowersList:': len(followed_user.followers.all())
-            , 'myuserfollowersList': len(user.followers.all()),
-         'theuseruserfollowList:': len(followed_user.followed.all())}), 200)
+
+    return make_response(jsonify({'ok':'followed'}), 200)
 
 
-@app.route("/user/unfollow/<int:followed_user_id>", methods=['POST'])
+@app.route("/user/follow/<int:followed_user_id>", methods=['DELETE'])
 @login_required
 def unfollow(followed_user_id):
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     followed_user = User.query.filter_by(id=followed_user_id).first()
     if not user or not followed_user:
@@ -240,7 +215,7 @@ def unfollow(followed_user_id):
 
     db.session.commit()
 
-    return 'done'
+    return make_response(jsonify({'ok':'unfollowed'}), 200)
 
 
 @app.route("/user/subscribe/<int:post_id>", methods=['POST'])
@@ -248,7 +223,7 @@ def unfollow(followed_user_id):
 def subscribe(post_id):
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     post = Travel.query.filter_by(id=post_id).first()
     follow = Follow.query.filter_by(follower_id=user_id, followed_id=post.user_id).first()
@@ -257,38 +232,36 @@ def subscribe(post_id):
     subInstance = Subscribe(post_id=post_id, subscriber_id=user_id, follow_id=follow.id)
     db.session.add(subInstance)
     db.session.commit()
-    return 'done'
+    return make_response(jsonify({'ok': 'subscribed'}), 200)
 
 
-@app.route("/user/unsubscribe/<int:post_id>", methods=['POST'])
+@app.route("/user/subscribe/<int:post_id>", methods=['DELETE'])
 @login_required
 def unsubscribe(post_id):
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     post = Travel.query.filter_by(id=post_id).first()
     if not user or not post:
         abort(404)
     Subscribe.query.filter_by(post_id=post_id, subscriber_id=user_id).delete(synchronize_session=False)
     db.session.commit()
-    return 'done'
+    return make_response(jsonify({'ok': 'unsubscribed'}), 200)
 
 
-@app.route("/user/edit/<int:post_id>", methods=['POST'])
+@app.route("/user/post/<int:post_id>", methods=['PUT'])
 @login_required
-def edit(post_id):
+def editpost(post_id):
     data = request.get_json()
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     post = Travel.query.filter_by(id=post_id).first()
     if not user or not post or int(post.user_id) != int(user_id):
         abort(404)
     subscribe = Subscribe.query.filter_by(post_id=post_id).all()
-    if not subscribe:
-        abort(404)
     newPost = createPost(data)
     post.title = newPost.title
     post.start_date = newPost.start_date
@@ -302,7 +275,7 @@ def edit(post_id):
         notification = Notification(subscribe_id=i.id)
         i.notifications.append(notification)
     db.session.commit()
-    return 'done '
+    return make_response(jsonify({'ok': 'edited'}), 200)
 
 
 @app.route("/user/notifications", methods=['GET'])
@@ -310,7 +283,7 @@ def edit(post_id):
 def getNotifications():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     if not user:
         abort(404)
@@ -320,33 +293,33 @@ def getNotifications():
     return make_response(jsonify(json_list), 200)
 
 
-@app.route("/user/deleteaccount", methods=['GET'])
+@app.route("/user", methods=['DELETE'])
 @login_required
 def deleteAccount():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id)
     if not user:
         abort(404)
     user.delete(synchronize_session=False)
     db.session.commit()
     logout_user()
-    return 'deleted but not tested yet'
+    return make_response(jsonify({'ok': 'deleted'}), 200)
 
 
-@app.route("/user/deletepost/<int:post_id>", methods=['POST'])
+@app.route("/user/post/<int:post_id>", methods=['DELETE'])
 @login_required
 def deletepost(post_id):
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     post = Travel.query.filter_by(id=post_id)
     if not post:
         abort(404)
     post.delete(synchronize_session=False)
     db.session.commit()
-    return 'done '
+    return make_response(jsonify({'ok': 'deleted'}), 200)
 
 
 @app.route("/user/postfeed", methods=['GET'])
@@ -354,7 +327,7 @@ def deletepost(post_id):
 def getpostfeed():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     userPosts = user.get_posts_as_list()
     for i in user.followed.all():
@@ -369,7 +342,6 @@ def searchuser(user_tosearch):
     user = User.query.filter_by(username=user_tosearch).first()
     if not user:
         abort(404)
-
     user_id = user.id
     return make_response(jsonify(user_id), 200)
 
@@ -379,7 +351,7 @@ def searchuser(user_tosearch):
 def getfollowers():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     if not user:
         abort(404)
@@ -392,23 +364,20 @@ def getfollowers():
 def getfollowings():
     user_id = current_user.get_id()
     if not user_id:
-        abort(404)
+        abort(403)
     user = User.query.filter_by(id=user_id).first()
     if not user:
         abort(404)
     return make_response(jsonify(user.getfollowings()), 200)
 
 
-@app.route("/user/search_on_map", methods=['POST'])
+@app.route("/user/search_on_map", methods=['GET'])
 @login_required
 def search_on_map():
     data = request.get_json()
-    # user_id = current_user.get_id()
-    # if not user_id:
-    #     abort(404)
-    # user = User.query.filter_by(id=user_id).first()
-    # if not user:
-    #     abort(404)
+    user_id = current_user.get_id()
+    if not user_id:
+        abort(403)
     if not data or not 'longitude' in data or not 'latitude' in data or not 'km' in data:
         abort(404)
     long = data['longitude']
